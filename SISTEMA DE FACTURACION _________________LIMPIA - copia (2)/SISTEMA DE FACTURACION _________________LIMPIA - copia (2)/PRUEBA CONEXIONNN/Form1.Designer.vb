7eefb388065ed20161ce7ec7f0717d3f7699d0b1
mario <jmuspeed@gmail.com>
2018-11-14 18:33:02 -0500

    todo

--- SISTEMA DE FACTURACION _________________LIMPIA - copia (2)/SISTEMA DE FACTURACION _________________LIMPIA - copia (2)/PRUEBA CONEXIONNN/Form1.Designer.vb
+++ SISTEMA DE FACTURACION _________________LIMPIA - copia (2)/SISTEMA DE FACTURACION _________________LIMPIA - copia (2)/PRUEBA CONEXIONNN/Form1.Designer.vb
@@ -34,6 +34,7 @@ Partial Class Form1
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         CType(Me.LinqServerModeSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -103,11 +104,19 @@ Partial Class Form1
         Me.Button6.Text = "Button6"
         Me.Button6.UseVisualStyleBackColor = True
         '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(695, 39)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 361)
+        Me.Panel1.TabIndex = 6
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(473, 185)
+        Me.ClientSize = New System.Drawing.Size(1039, 458)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
@@ -131,4 +140,5 @@ Partial Class Form1
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
+    Friend WithEvents Panel1 As Panel
 End Class
